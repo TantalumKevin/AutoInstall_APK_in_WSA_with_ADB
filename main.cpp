c43e55248226ed087c42dbe5418a832aadb92882
Kevin&Surface <tantalum_zhao@qq.com>
2022-06-18 00:58:45 +0800

    查看迟迟没有输出的原因（后期增加信息显示）

--- main.cpp
+++ main.cpp
@@ -23,17 +23,23 @@ int main(int argc, char** argv)
     char cmd[1024] = {0}; 
     char out1[1024] = "connected to 127.0.0.1:58526\n",out2[1024] = "already connected to 127.0.0.1:58526\n";
     strcat(ptr, "adb connect \"127.0.0.1:58526\"");
-    bool flag =true;
-    while(flag)
+    int flag =1;
+    while(flag>0)
         if ((file = _popen(ptr, "r")) != NULL)
             //执行一次指令
             while (fgets(cmd, 1024, file) != NULL)
-                //读一行输出
-                //cout << cmd;
+            {    //读一行输出
+                cout << flag;
                 //cout <<!strcmp(cmd,out1)<<!strcmp(cmd,out2)<<endl;
                 if(!strcmp(cmd,out1)||!strcmp(cmd,out2))
-                    flag = !flag;
-    cout<<1;
+                    flag = 0;
+                else if(flag>=50)
+                    flag = -1;
+                else
+                    flag++;
+            }
+    if (flag==-1)
+        cout<<"Here"<<endl;
     _pclose(file);
     return 0;
 }
